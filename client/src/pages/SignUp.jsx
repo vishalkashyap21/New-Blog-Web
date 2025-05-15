c9cbdd4de29b5d25bf869de92aa5863101c461c7
Jatin Kashyap <vishalkashyap2122002@gmail.com>
2025-05-15 22:39:39 +0530

    Change

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -10,7 +10,7 @@ import { RouteSignIn } from '@/helpers/RouteName'
 import { Link, useNavigate } from 'react-router-dom'
 import { getEvn } from '@/helpers/getEnv'
 import { showToast } from '@/helpers/showToast'
-import GoogleLogin from '@/components/GoogleLogin'
+
 
 const SignUp = () => {
 
@@ -57,7 +57,7 @@ const SignUp = () => {
             <Card className="w-[400px] p-5">
                 <h1 className='text-2xl font-bold text-center mb-5'>Create Your Account</h1>
                 <div className=''>
-                    <GoogleLogin />
+                  
                     <div className='border my-5 flex justify-center items-center'>
                         <span className='absolute bg-white text-sm'>Or</span>
                     </div>
